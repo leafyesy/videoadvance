2da0f5709121ab1c43eed0127ecb6ec9bbd3069b
leafyesy <125021540@qq.com>
2020-01-08 00:45:54 +0800

    [opengl]save

--- AndroidVideoAdvance/opengldemo/src/main/java/com/leafye/opengldemo/MainActivity.kt
+++ AndroidVideoAdvance/opengldemo/src/main/java/com/leafye/opengldemo/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.leafye.opengldemo
 
+import android.opengl.EGL14
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
@@ -8,5 +9,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        startGl()
+    }
+
+    private fun startGl() {
+        val eglGetDisplay = EGL14.eglGetDisplay(EGL14.EGL_DEFAULT_DISPLAY)
+
     }
 }
